b4e32672417f3208b2c02969f6909a6d99788a0a
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2024-01-18 08:36:32 -0800

    Create Django Document Model

--- s3_backend/pdf_handler_api/models.py
+++ s3_backend/pdf_handler_api/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+    date = models.DateField()
+    pdf_file = models.FileField(upload_to="pdfs/")
+
+    def __str__(self):
+        return self.name
