0839632cce2f1adc4bfbcab5baf147cd1bda8e46
Anthony Piegaro <anthonypiegaro2@yahoo.com>
2024-01-18 14:31:17 -0800

    Add form validation

--- s3-frontend/src/components/PDF/PDF.js
+++ s3-frontend/src/components/PDF/PDF.js
@@ -9,15 +9,34 @@ const PDF = () => {
     const [date, setDate] = useState(dayjs());
     const [name, setName] = useState("");
     const [file, setFile] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [inputsValidState, setInputsValidState] = useState({"name": true, "date": true, "file": true});
+
+    const validate = () => {
+        const fileStatus = Boolean(file);
+        const nameStatus = Boolean(name);
+        const dateStatus = date.isValid();
+
+        setInputsValidState({
+            "name": nameStatus,
+            "date": dateStatus,
+            "file": fileStatus
+        });
+
+        console.log(fileStatus && nameStatus && dateStatus);
+
+        return (fileStatus && nameStatus && dateStatus)
+    }
 
     return (
         <div className="pdf-page">
             <div className="header">Add PDF</div>
             <form>
-                <TextField label="PDF Name" variant="outlined" value={name} onChange={(value) => setName(value)} />
+                <TextField label="PDF Name" variant="outlined" value={name} onChange={(event) => setName(event.target.value)} />
                 <PDFDatePicker date={date} setDate={setDate} />
                 <PDFInput file={file} setFile={setFile} />
-                <Button variant="outlined" sx={{borderColor: "black", color: "black"}}>Submit</Button>
+                <Button variant="outlined" sx={{borderColor: "black", color: "black"}} onClick={validate}>Submit</Button>
             </form>
         </div>
     );
